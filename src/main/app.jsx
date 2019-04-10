b1f421dcec632699311f9dc59c6c7a234194974a
Wellici <welliciaraujo2017@gmail.com>
2019-04-10 11:15:05 -0300

    ConfPreparação do ambiente de desenvolvimento React

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+import NavBar from '../navbar/navbar';
+
+export default props => (
+  <div>
+    <NavBar/>
+  </div>
+)
---
+++ src/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import '../css/index.css';
+
+import React from 'react';
+
+export default props => (
+  <div className="navbar-menu">
+
+  </div>
+)
